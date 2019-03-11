c5ed3068bd3edc257c3fa157a8855736b4846d56
BIT Student <classroom@bgit.rs>
2019-03-11 14:46:25 +0100

    completed

--- Festival/festival.js
+++ Festival/festival.js
@@ -1,74 +1,74 @@
+//"use strict"
 
+(function () {
 
-(function(){
+    function Genre(name) {
+        this.name = name;
 
-    function Genre(name){
-        this.name= name;
-
-        this.getData = function(){
+        this.getData = function () {
             return this.name.charAt(0).toUpperCase() + "" + this.name[this.name.length - 1].toUpperCase();
         };
     };
-    
-        
-    function Movie(title, length, genre){
+
+
+    function Movie(title, length, genre) {
         this.title = title;
         this.length = length;
-    
-        this.getData = function(){
-            return this.title + ", " +  this.length + "min, " + genre.getData();
-        };
-        
-    };
-  
 
-    function Program(date){
-        this.date = new Date(date);
+        this.getData = function () {
+            return this.title + ", " + this.length + "min, " + genre.getData();
+        };
+
+    };
+
+
+    function Program(date) {
+        this.date = new Date(date).toLocaleDateString();
         this.movieList = [];
         this.totalNumOfMovies = 0;
 
-        this.addMovie = function(movie){
+        this.addMovie = function (movie) {
             this.movieList.push(movie);
             this.totalNumOfMovies++;
         };
 
-        this.getData = function(){
+        this.getData = function () {
             var movieLength = 0;
             var formatedStr = "";
-            
-            for(var i = 0; i < this.movieList.length; i++){
-                movieLength+=this.movieList[i].length;
-                formatedStr+=this.movieList[i].getData() + "\n"
-                
+
+            for (var i = 0; i < this.movieList.length; i++) {
+                movieLength += this.movieList[i].length;
+                formatedStr += this.movieList[i].getData() + "\n"
+
             };
-            
-                return this.date + ", " + "program duration " + movieLength + "\n" + formatedStr;
+
+            return this.date + ", " + "program duration " + movieLength + "\n" + formatedStr;
         }
     };
 
 
-    function Festival(name){
+    function Festival(name) {
         this.name = name;
-        this.programList  = [];
-        
+        this.programList = [];
 
-        this.addProgram = function(program){
+
+        this.addProgram = function (program) {
             this.programList.push(program);
-          
+
         };
 
-        this.getData = function(){
+        this.getData = function () {
 
             var sentence = 0;
-                var sentenceTwo = "";
-                for(var i = 0; i < this.programList.length; i++){
-                    sentence+=this.programList[i].totalNumOfMovies;
-                    sentenceTwo+= this.programList[i].getData() + "\n";
-                   
-                };
-               console.log( this.name + " has " + sentence + " movie titles \n" + sentenceTwo)
+            var sentenceTwo = "";
+            for (var i = 0; i < this.programList.length; i++) {
+                sentence += this.programList[i].totalNumOfMovies;
+                sentenceTwo += this.programList[i].getData() + "\n";
+
+            };
+            console.log(this.name + " has " + sentence + " movie titles \n" + sentenceTwo)
         }
-        
+
     };
 
 
@@ -78,16 +78,16 @@
     var comedy = new Genre("comedy");
 
 
-        function createMovie(title, length, genre){
-            return new Movie(title, length, genre)
-        }
+    function createMovie(title, length, genre) {
+        return new Movie(title, length, genre)
+    }
 
     var movie1 = new Movie("Johnwick", 123, action);
     var movie2 = new Movie("Luckyone", 120, drama);
     var movie3 = new Movie("Indianajones", 135, adventure);
     var movie4 = new Movie("JonnyEnglish", 180, comedy);
 
-    function createProgram(date){
+    function createProgram(date) {
         return new Program(date)
     }
     var program1 = new Program("08.10.2018");
@@ -107,8 +107,8 @@
     // console.log(program1.addMovie(movie1));
     // console.log(program2.addMovie(movie3));
     // console.log(fest.addProgram(program1))
-     console.log(fest.getData());
-   
+    console.log(fest.getData());
+
 
 
 
